4d44bc030aeb981e1caa4398bc2df0a1d39ed3ff
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2020-12-17 10:29:37 +0100

    Removed creditentials, eggs.

--- edomus/constantes.inc.php
+++ edomus/constantes.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-    define("API_USER",  'p5o5jy'); 
-    define("API_SECRET", 'hOfMt9jBKB8kz3UE'); 
-    define("IPBOX",'10.129.137.184');
+    define("API_USER",  'YOUR API USER'); 
+    define("API_SECRET", 'YOUR API SECRET'); 
+    define("IPBOX",'YOUR IP');
 
